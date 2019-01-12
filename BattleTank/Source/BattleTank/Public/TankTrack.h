a14c99130b212ac0d64ce7b2bbc9ed51d5264a5b
darbitman <d.arbitman@gmail.com>
2019-01-11 19:03:48 -0800

    Added force generators to tank tracks

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -19,5 +19,7 @@ public:
   UFUNCTION(BlueprintCallable, Category = Input)
   void SetThrottle(float Throttle);
 
-  // TODO clamp actual throttle value so player can't overdrive
+  // Max force per track, in Newtons
+  UPROPERTY(EditDefaultsOnly)
+  float TrackMaxDrivingForce = 400000;  // Assume 40 ton tank, and 1g acceleration
 };
