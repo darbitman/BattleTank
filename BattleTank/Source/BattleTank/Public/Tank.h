766cfe28d683ad1ffda7aeb2d11a8a25428f53ba
darbitman <d.arbitman@gmail.com>
2019-01-11 20:53:39 -0800

    TankMovementComponent supports forward movement using W key

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -33,9 +33,6 @@ public:
 protected:
   UTankAimingComponent* TankAimingComponent = nullptr;
 
-  UPROPERTY(BlueprintReadOnly)
-  UTankMovementComponent* TankMovementComponent = nullptr;
-
 private:
 	// Sets default values for this pawn's properties
 	ATank();
--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -6,8 +6,11 @@
 #include "GameFramework/NavMovementComponent.h"
 #include "TankMovementComponent.generated.h"
 
+// Forward declarations
+class UTankTrack;
+
 /**
- * 
+ * Responsible for driving the tank tracks
  */
 UCLASS(meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
@@ -15,7 +18,13 @@ class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 	GENERATED_BODY()
 
 public:
+  UFUNCTION(BlueprintCallable, Category = Setup)
+  void Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
+
   UFUNCTION(BlueprintCallable, Category = Input)
   void IntendMoveForward(float Throw);
 
+private:
+  UTankTrack* LeftTrack = nullptr;
+  UTankTrack* RightTrack = nullptr;
 };
