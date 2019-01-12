644e0d8c6062fe182964c36d90b5c30b8bcad639
darbitman <d.arbitman@gmail.com>
2019-01-12 12:23:47 -0800

    RequestDirectMove override in order to intercept the MoveVelocity vector so that the TankAIController can perform the movement instead of the engine

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -32,3 +32,11 @@ void UTankMovementComponent::IntendTurnLeft(float Throw)
   RightTrack->SetThrottle(Throw);
   // TODO prevent double-speed due to dual control use
 }
+
+void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
+{
+  // No need to call Super as we're replacing the functionality
+  auto TankName = GetOwner()->GetName();
+  auto MoveVelocityString = MoveVelocity.ToString();
+  UE_LOG(LogTemp, Warning, TEXT("%s vectoring to %s"), *TankName, *MoveVelocityString);
+}
