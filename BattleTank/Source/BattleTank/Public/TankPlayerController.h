6a3620faec960c91cfd215c6cb00f2df81d7d82d
darbitman <d.arbitman@gmail.com>
2019-01-13 08:16:46 -0800

    Added hook into crosshair color

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -14,17 +14,18 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 
-protected:
-  // Called when the game starts or when spawned
-  virtual void BeginPlay() override;
-
 public:
   // Called every frame
   virtual void Tick(float DeltaTime) override;
 
-private:
+protected:
+  // Called when the game starts or when spawned
+  virtual void BeginPlay() override;
+
+  UFUNCTION(BlueprintCallable, Category = "Setup")
   ATank* GetControlledTank() const;
 
+private:
   // Start the tank moving the barrel so that a shot would hit where
   // the crosshair intersects the world
   void AimTowardsCrosshair();
