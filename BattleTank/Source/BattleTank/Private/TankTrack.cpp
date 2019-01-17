767e6a3ecff4bb323210bc44c296a5d073f5c3a3
darbitman <d.arbitman@gmail.com>
2019-01-16 20:08:16 -0800

    Added a force to counteract sliding, however there's a bug and the tank rocks back and forth violently.

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -2,6 +2,29 @@
 
 #include "TankTrack.h"
 
+UTankTrack::UTankTrack()
+{
+  PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTankTrack::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+  Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+  auto TankVelocity = GetComponentVelocity();
+  auto TankRightVector = GetRightVector();
+  auto SlippageSpeed = FVector::DotProduct(TankVelocity, TankRightVector);
+
+  // Work out the required acceleration this frame to correct
+  auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+  
+  // Calculate the apply sideways force
+  auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+  auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2.0;  // Divide by two since 2 tracks
+  //CorrectionForce.Z = 0.0;
+  TankRoot->AddForceAtLocation(CorrectionForce, GetComponentLocation());
+}
+
 void UTankTrack::SetThrottle(float Throttle)
 {
   auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
